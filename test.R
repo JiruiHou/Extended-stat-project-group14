9aa5530a56b047efd2a5bbf3f4ec4297cb6ce32b
Ben Sturm <bensturm@Bens-MacBook-Air-9.local>
2026-09-25 12:59:50 +0100

    name

--- test.R
+++ test.R
@@ -8,8 +8,8 @@ head(music_raw[10:20])
 #PSEUDOCODE
 
 #preprocessing
-  #remove the initial metadata
-  #remove the supplementary info from lines starting with #
+  #remove the initial metadata -Ben
+  #remove the supplementary info from lines starting with # -Ben
   #separate pieces on the || symbol, replacing current blank line   
   #separate lines on the | symbol, replacing current newline (except last line, which I think we can detect by presence of a blank next line?)
   #separate notes on the space symbol
