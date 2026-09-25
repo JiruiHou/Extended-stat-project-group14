2b36d2bc2604e15a96d5588e25fc9c3ea1a805f3
JIRUI HOU <s2915675@ed.ac.uk>
2026-09-25 19:23:43 +0800

    download file

--- test.R
+++ test.R
@@ -1,2 +1,7 @@
-x<-3+3
-print(x)
+#Change your own file address
+setwd("E:/extended statistics/Practical-1") 
+music_raw <- readLines("palestrina.txt")
+
+#(4)
+#music_raw[10:20,]
+#head(music_raw[10:20,])
