a7fecd0bc3e16899543231f7a0111b2fec6f37ce
Ben Sturm <bensturm@Bens-MacBook-Air-9.local>
2026-09-25 12:53:23 +0100

    more templating

--- test.R
+++ test.R
@@ -1,18 +1,19 @@
 
-#Change your own file address
-setwd("/Users/bensturm/Extended Statistical Programming Lab 1/Extended-stat-project-group14")
+
 music_raw <- readLines("palestrina.txt")
 
-<<<<<<< HEAD
+
 head(music_raw[10:20])
 
 #PSEUDOCODE
 
 #preprocessing
-
-=======
-#(4)
-#music_raw[10:20,]
-#head(music_raw[10:20,])
->>>>>>> e7bad9f8a3f16aeafb8283ea52f2af0779900912
+  #remove the initial metadata
+  #remove the supplementary info from lines starting with #
+  #separate pieces on the || symbol
+  #separate lines on the | symbol
+  #separate notes on the space symbol
 #model
+
+#generation
+
