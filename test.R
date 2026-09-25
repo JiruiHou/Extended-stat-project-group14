725af42f8b16ab94dd6888d9f767e2caf6c4ac14
Ben Sturm <bensturm@Bens-MacBook-Air-9.local>
2026-09-25 12:57:38 +0100

    more detailed template

--- test.R
+++ test.R
@@ -10,8 +10,8 @@ head(music_raw[10:20])
 #preprocessing
   #remove the initial metadata
   #remove the supplementary info from lines starting with #
-  #separate pieces on the || symbol
-  #separate lines on the | symbol
+  #separate pieces on the || symbol, replacing current blank line   
+  #separate lines on the | symbol, replacing current newline (except last line, which I think we can detect by presence of a blank next line?)
   #separate notes on the space symbol
 #model
 
